6f8f6120525ed2e85d6686ab7f09ebbddfe78da8
HubertNastaly <56291339+HubertNastaly@users.noreply.github.com>
2023-03-11 15:59:27 +0100

    Fetch contacts from endpoint (#4)
    * Fetch contacts from endpoint
    
    * Extract Gender type

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,30 @@
+type Gender = 'Male' | 'Female'
+
 export interface Contact {
   id: number
   firstName: string
   lastName: string
   email: string
-  gender: 'Male' | 'Female'
+  gender: Gender
   avatar: string
+}
+
+export interface ApiContact {
+  id: number
+  first_name: string
+  last_name: string
+  email: string
+  gender: Gender
+  avatar: string
+}
+
+export function asContact(apiContact: ApiContact): Contact {
+  return {
+    id: apiContact.id,
+    firstName: apiContact.first_name,
+    lastName: apiContact.last_name,
+    email: apiContact.email,
+    gender: apiContact.gender,
+    avatar: apiContact.avatar
+  }
 }
