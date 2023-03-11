b57ab7e42503b453b11e30a014eaeba2688406e2
HubertNastaly <56291339+HubertNastaly@users.noreply.github.com>
2023-03-11 19:55:35 +0100

    Improve styling and remove styled-components (#9)

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -2,6 +2,6 @@ import { TextField, TextFieldProps } from '@mui/material'
 
 export const Searchbar = (textFieldProps: TextFieldProps) => {
   return (
-    <TextField {...textFieldProps} fullWidth variant="outlined" />
+    <TextField {...textFieldProps} label="Search" fullWidth variant="outlined" color="primary" />
   )
 }
