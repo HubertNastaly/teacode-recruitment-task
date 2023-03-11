712a3e3e8333bfa49de55db38e9508982ece88f2
Hubert Nastały <h.nastaly@gmail.com>
2023-03-11 17:03:06 +0100

    Implement searching contacts

--- src/components/ContactsList.tsx
+++ src/components/ContactsList.tsx
@@ -1,21 +1,40 @@
 import { List } from '@mui/material'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import styled from 'styled-components'
+import debounce from 'lodash.debounce'
 
 import { ContactsListItem } from './ContactsListItem'
 import { Searchbar } from './Searchbar'
 import { Contact } from '../types'
+
+const DELAY = 200 //ms
 
 interface Props {
   allContacts: Contact[]
 }
 
 export const ContactsList = ({ allContacts }: Props) => {
-  const [filteredContacts] = useState(allContacts)
+  const [filteredContacts, setFilteredContacts] = useState(allContacts)
+
+  const handleSearchChange = useCallback(
+    debounce(
+      (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        const searchText = event.target.value.toLowerCase()
+
+        if(searchText === '') {
+          setFilteredContacts(allContacts)
+          return
+        }
+
+        setFilteredContacts(filterContacts(allContacts, searchText))
+      },
+      DELAY
+    )
+    , [])
 
   return (
     <>
-      <Searchbar  />
+      <Searchbar onChange={handleSearchChange} />
       <ListStyled>
         {filteredContacts.map(contact => (
           <ContactsListItem
@@ -26,6 +45,12 @@ export const ContactsList = ({ allContacts }: Props) => {
       </ListStyled>
     </>
   )
+}
+
+function filterContacts(contacts: Contact[], searchText: string) {
+  return contacts.filter(({ firstName, lastName, email }) => {
+    return [firstName, lastName, email].some(text => text.toLowerCase().includes(searchText))
+  })
 }
 
 const ListStyled = styled(List)`
