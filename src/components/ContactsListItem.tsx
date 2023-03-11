5140345970ea2f32ae9b738c1850671c667d60dc
Hubert Nastały <h.nastaly@gmail.com>
2023-03-11 19:24:07 +0100

    Toggle checkbox on list item click

--- src/components/ContactsListItem.tsx
+++ src/components/ContactsListItem.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Checkbox, ListItem, ListItemAvatar, ListItemText } from '@mui/material'
+import { Avatar, Checkbox, ListItem, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material'
 import React from 'react'
 import styled from 'styled-components'
 import { Contact } from '../types'
@@ -12,15 +12,17 @@ interface Props {
 export const ContactsListItemComponent = ({ contact, onCheckChange, checked }: Props) => {
   return (
     <ListItemStyled
-      secondaryAction={<Checkbox edge="end" checked={checked} onChange={(_, checked) => onCheckChange(contact.id, checked)} />}
+      secondaryAction={checked && <Checkbox edge="end" checked={true} />}
     >
-      <ListItemAvatar>
-        <Avatar src={contact.avatar} />
-      </ListItemAvatar>
-      <ListItemText
-        primary={`${contact.firstName} ${contact.lastName}`}
-        secondary={contact.email}
-      />
+      <ListItemButton onClick={() => onCheckChange(contact.id, !checked)}>
+        <ListItemAvatar>
+          <Avatar src={contact.avatar} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={`${contact.firstName} ${contact.lastName}`}
+          secondary={contact.email}
+        />
+      </ListItemButton>
     </ListItemStyled>
   )
 }
